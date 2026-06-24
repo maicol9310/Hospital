942e02c61a3acb1cc3300b07563e5f166dc6e0a0
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2026-06-24 13:06:47 -0500

    feat(order): create command and validation fluent in application

---
+++ src/Hospital.Application/Orders/Commands/CreateOrderResponse.cs
@@ -0,0 +1,9 @@
+﻿using Hospital.Domain.Enums;
+
+namespace Hospital.Application.Orders.Commands.CreateOrder;
+
+public sealed record CreateOrderResponse(
+    Guid Id,
+    decimal Total,
+    OrderStatus Status
+);
--- src/Hospital.Domain/Orders/Order.cs
+++ src/Hospital.Domain/Orders/Order.cs
@@ -1,6 +1,5 @@
 ﻿using Hospital.Domain.Enums;
-
-namespace Hopsital.Domain.Orders;
+using Hospital.Domain.Orders;
 
 public class Order
 {
@@ -8,25 +7,41 @@ public class Order
 
     public string CustomerName { get; private set; } = string.Empty;
 
-    public decimal Total { get; private set; }
-
     public DateTime CreatedAt { get; private set; }
+
+    public DateTime? ProcessedAt { get; private set; }
 
     public OrderStatus Status { get; private set; }
 
+    private readonly List<OrderItem> _items = [];
+
+    public IReadOnlyCollection<OrderItem> Items => _items.AsReadOnly();
+
+    public decimal Total => _items.Sum(x => x.SubTotal);
+
     private Order() { }
 
-    public Order(string customerName, decimal total)
+    public Order(string customerName)
     {
         Id = Guid.NewGuid();
         CustomerName = customerName;
-        Total = total;
         CreatedAt = DateTime.UtcNow;
         Status = OrderStatus.Pending;
+    }
+
+    public void AddItem(string product, int quantity, decimal price)
+    {
+        _items.Add(new OrderItem(
+            Guid.NewGuid(),
+            Id,
+            product,
+            quantity,
+            price));
     }
 
     public void MarkAsProcessed()
     {
         Status = OrderStatus.Processed;
+        ProcessedAt = DateTime.UtcNow;
     }
 }
