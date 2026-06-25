5ae2ea1f48ed7d88c84f06f002251128a7ef5298
Jan Michael Sanchez Sierra <maicol_931028@hotmail.com>
2026-06-25 16:13:16 -0500

    feat(sharedkernel): add MediatR validation pipeline behavior

---
+++ src/Hospital.Application/Queries/Orders/GetOrderById/GetOrderByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Hospital.Application.Queries.Orders.GetOrderById
+{
+    public class GetOrderByIdQueryValidator: AbstractValidator <GetOrderByIdQuery>
+    {
+       public GetOrderByIdQueryValidator() 
+       {
+            RuleFor(x => x.Id)
+                .NotEmpty();
+        }
+    }
+}
